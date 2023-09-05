b5c5decea2ed9c0aa6ebbf5980e57d25456233ce
Morakinyo <kielsweet2003@yahoo.com>
2023-09-05 10:39:24 +0100

    Boldening the Legend

--- Plot3.R
+++ Plot3.R
@@ -20,10 +20,12 @@ data <- data[data$Date %in% c("1/2/2007","2/2/2007"), ]
 plot(time_axis, data[, 7], xlab = "", ylab = "Energy sub metering", type = "l")
 lines(time_axis, data[, 8], col = "red")
 lines(time_axis, data[, 9], col = "blue")
-
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       col = c("black","red","blue"), lty = 1)
-
+legend("topright",
+       legend = c(expression(bold('Sub_metering_1')), 
+                  expression(bold('Sub_metering_2')), 
+                  expression(bold('Sub_metering_3'))),
+                  
+       col = c('black', 'red','blue'), lty = 1, cex = 0.)
 # Saving data to PNG
 dev.copy(png,"plot3.png", width=480, height=480)
 dev.off()
